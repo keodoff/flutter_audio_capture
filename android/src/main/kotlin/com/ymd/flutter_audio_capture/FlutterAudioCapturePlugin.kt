ac9ab1f3dbb4d8554b7a8717f9768fcdfed9b59f
Anashuman Singh <anash.cheema@gmail.com>
2025-02-14 09:50:00 +0530

    chore: fix build

--- android/src/main/kotlin/com/ymd/flutter_audio_capture/FlutterAudioCapturePlugin.kt
+++ android/src/main/kotlin/com/ymd/flutter_audio_capture/FlutterAudioCapturePlugin.kt
@@ -8,7 +8,6 @@ import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** FlutterAudioCapturePlugin */
 public class FlutterAudioCapturePlugin: FlutterPlugin, MethodCallHandler {
@@ -20,21 +19,6 @@ public class FlutterAudioCapturePlugin: FlutterPlugin, MethodCallHandler {
     val messenger = flutterPluginBinding.getBinaryMessenger()
     EventChannel(messenger, this.audioCaptureStreamHandler.eventChannelName).setStreamHandler(this.audioCaptureStreamHandler)
     MethodChannel(messenger, methodChannelName).setMethodCallHandler(this)
-  }
-
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-    }
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
